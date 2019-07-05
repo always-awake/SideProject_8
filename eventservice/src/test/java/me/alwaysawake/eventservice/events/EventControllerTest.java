9f8d0f5016f8d4e07a9847da081d3dbab4ba5c72
leemirim <leemirim9654@gmail.com>
2019-07-05 22:28:08 +0900

    EventControllerTest의 createdEventTest() 테스트에 주석 추가

--- eventservice/src/test/java/me/alwaysawake/eventservice/events/EventControllerTest.java
+++ eventservice/src/test/java/me/alwaysawake/eventservice/events/EventControllerTest.java
@@ -21,11 +21,12 @@ public class EventControllerTest {
 
     @Test
     public void createEventTest() throws Exception {
+        // 입력값들을 전달하면, JSON 응답으로 201이 나오는지 확인.
         mockMvc.perform(
                 post("/events")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)  // post 요청 시, content에 JSON을 담아서 보내고 있다.
+                .accept(MediaTypes.HAL_JSON) // Accept 헤더: 나는 HAL_JSON 형식의 응답을 원한다.
                 )
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated()); // 최종 상태 코드는 201을 받고 싶다.
     }
 }
